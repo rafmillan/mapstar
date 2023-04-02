77ecdbdb29415e334ec13c915e2715feaa66afa8
rafmillan <rafael.k.millan@gmail.com>
2023-04-02 01:17:49 -0700

    on startup ath is 0

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,10 @@ import React from "react";
 import Modal from "./Modal"
 
 export default function Header({hiScore, scores}) {
-    let ath = Math.max(...scores)
+    let ath = 0
+    if (scores.length > 0) {
+        ath = Math.max(...scores)
+    }
     return (
         <div className="flex flex-col-2 justify-between pb-1 tracking-wider">
             <div className="px-5 text-3xl font-semibold">
