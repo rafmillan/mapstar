f92c44673789773d0a1d8d0d20d1055482cbe385
rafmillan <rafael.k.millan@gmail.com>
2023-04-02 01:19:03 -0700

    updated footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Footer() {
 	return (
-        <footer className="first-letter:text-xs bottom-0 flex fixed allign-center left-1/2 transform -translate-x-1/2" >
+        <footer className="flex fixed allign-center left-1/2 transform -translate-x-1/2 mt-10" >
             (C) Raf Millan - 2023
         </footer>
 	)
