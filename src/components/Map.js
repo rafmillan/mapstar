b5e2a0fb3f3bbab74e7460d311f2f2e28a28d383
rafmillan <rafael.k.millan@gmail.com>
2023-04-01 23:31:31 -0700

    removed duplicates until all unique locations are visited

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,20 +7,6 @@ export default function Map({data, answer}) {
             <div className="">
                 <MapViewer imageId={data[answer].imageId} />
             </div>
-            {/* <div className="flex justify-between">
-                <button className="w-1/2 py-1 mx-1 my-2 text-white rounded border bg-slate-800 hover:bg-slate-900"
-                    onClick={prevLocation}
-                // disabled={index === 0}
-                >
-                    prev
-                </button>
-                <button className="w-1/2 py-1 mx-1 my-2 text-white rounded border bg-slate-800 hover:bg-slate-900"
-                    onClick={nextLocation}
-                // disabled={index === data.length - 1}
-                >
-                    next
-                </button>
-            </div> */}
         </div>
     );
 }
