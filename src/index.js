352142ed712708e87f4771f94a804b4dcc110283
rafmillan <rafael.k.millan@gmail.com>
2023-04-02 22:31:33 -0700

    Updated to react18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+)
