deda1d22cf94a5e89fd5578c3c6e8b5efdd21c49
rafmillan <rafael.k.millan@gmail.com>
2023-04-02 20:55:47 -0700

    updated color scheme. Implemnted local storage to keep track of high score

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,15 @@
 import React from "react";
 import Modal from "./Modal"
 
-export default function Header({hiScore, scores}) {
-    let ath = 0
-    if (scores.length > 0) {
-        ath = Math.max(...scores)
-    }
+export default function Header({streak, hiScore}) {
     return (
         <div className="flex flex-col-2 justify-between pb-1 tracking-wider">
             <div className="px-5 text-3xl font-bold">
                 mapstar
             </div>
-            <Modal currentStreak={hiScore} allTimeHigh={ath}/>
+            <Modal currentStreak={streak} allTimeHigh={hiScore}/>
             <div className="px-5 text-3xl font-bold">
-                streak: {hiScore}
+                streak: {streak}
             </div>
             
         </div>
